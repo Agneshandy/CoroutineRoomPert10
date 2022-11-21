0d8724c77b03b5ac99bcf405fb43e1c299cc2340
Agneshandy <agneshandy21@gmail.com>
2022-11-21 10:52:15 +0700

    menambahkan file Coroutine Room PBM Pertemuan 10

---
+++ app/src/main/java/com/android/example/coroutineroom/NoteViewModel.kt
@@ -0,0 +1,33 @@
+package com.android.example.coroutineroom
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.android.example.coroutineroom.catatan.Note
+import com.android.example.coroutineroom.catatan.NoteRepository
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+    private var repository: NoteRepository =
+        NoteRepository(application)
+    private var allNotes: LiveData<List<Note>> = repository.getAllNotes()
+
+    fun insert(note: Note) {
+        repository.insert(note)
+    }
+
+    fun update(note: Note) {
+        repository.update(note)
+    }
+
+    fun delete(note: Note) {
+        repository.delete(note)
+    }
+
+    fun deleteAllNotes() {
+        repository.deleteAllNotes()
+    }
+
+    fun getAllNotes(): LiveData<List<Note>> {
+        return allNotes
+    }
+}
